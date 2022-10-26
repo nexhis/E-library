7c49fe6dbcfaee611f547b1566e96f05a9a9f20f
Kpharshita <113061321+Kpharshita@users.noreply.github.com>
2022-10-25 20:57:20 -0500

    Update models.py

--- elibrary/models.py
+++ elibrary/models.py
@@ -12,5 +12,16 @@ class Books(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.CharField(max_length=100)
 
+    class BookInstance(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,help_text="Book unique id across the Library")
+    book=models.ForeignKey('Book', on_delete=models.CASCADE,null=True)
+    book_number=models.PositiveIntegerField(null=True,help_text="Book number for books of the save kind")
+    Is_borrowed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.id} {self.book}"
+
+def get_returndate():
+    return datetime.today() + timedelta(days=15)
     def __str__(self):
         return self.title
