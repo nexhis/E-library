13b7761726e67ed10110e26138210961c4e6e3fd
chintak <chintakmanojkumar.joshi@slu.edu>
2022-11-02 19:07:14 -0500

    fixing some bugs

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=100)
+
+
