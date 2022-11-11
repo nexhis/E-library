726a6e29ff90210ce4ab80c2cbe067d53ca0e4b2
nexhis <nexhi.sula@slu.edu>
2022-11-11 09:43:46 -0600

    Adding new test cases

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -14,3 +14,57 @@ class HomepageTests(SimpleTestCase):
     def test_template_name_correct(self):
         response = self.client.get(reverse("library-homepage"))
         self.assertTemplateUsed(response, "elibrary/home.html")
+
+
+class LoginpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/login/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("login"))
+        self.assertTemplateUsed(response, "users/loginpage.html")
+
+class LogoutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.post("/logout/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.post(reverse("logout"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.post(reverse("logout"))
+        self.assertTemplateUsed(response, "users/logout.html")
+
+class RegisterpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.post("/register/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.post(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.post(reverse("register"))
+        self.assertTemplateUsed(response, "users/registerpage.html")
+
+
+class AboutuspageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.post("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("library-about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("library-about"))
+        self.assertTemplateUsed(response, "elibrary/about.html")
