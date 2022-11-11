fffae56bf43e6b21232978bd5fdfbf28d2568720
kpharshita <116698851+kpharshita@users.noreply.github.com>
2022-11-11 00:20:48 -0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,25 @@
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Book unique id across the Library")
+    book = models.ForeignKey('Book', on_delete=models.CASCADE, null=True)
+    book_number = models.PositiveIntegerField(null=True, help_text="Book number for books of the save kind")
+    Is_borrowed = models.BooleanField(default=False)
+    def __str__(self):
+            return f"{self.id} {self.book}"
+
+def get_returndate():
+    return datetime.today() + timedelta(days=15)
---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("view_books/", views.view_books, name="view_books"),
+    path("issue_book/", views.issue_book, name="issue_book"),
+    path("view_issued_book/", views.view_issued_book, name="view_issued_book"),
+    path("student_issued_books/", views.student_issued_books, name="student_issued_books"),
+
+]
