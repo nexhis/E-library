afe4b090e095a0f53064379484695b51e4b9af5c
chintak <chintakmanojkumar.joshi@slu.edu>
2022-11-18 17:47:48 -0600

    made some changes on welcome page

--- tests/test_register_url.py
+++ tests/test_register_url.py
@@ -11,18 +11,7 @@ def test_views(client, param):
     temp_url = urls.reverse(param)
     resp = client.get(temp_url)
     assert resp.status_code == 200
-
-
-# @pytest.mark.django_db
-# def test_signup(client, user_data):
-#     user_model = get_user_model()
-#     assert user_model.objects.count() == 0
-#     signup_url = urls.reverse('register')
-#     resp = client.post(signup_url, user_data)
-#     assert user_model.objects.count() == 1
-#     assert resp.status_code == 302
-
-
+    
 @pytest.mark.django_db
 def test_user_login(client, create_test_user, user_data):
     user_model = get_user_model()
@@ -31,11 +20,3 @@ def test_user_login(client, create_test_user, user_data):
     resp = client.post(login_url, data=user_data)
     assert resp.status_code == 302
     assert resp.url == urls.reverse('library-homepage')
-
-
-# @pytest.mark.django_db
-# def test_user_logout(client, authenticated_user):
-#     logout_url = urls.reverse('logout')
-#     resp = client.get(logout_url)
-#     assert resp.status_code == 302
-#     assert resp.url == urls.reverse('library-homepage')
