c3cccf6e76d23b3ba4ae52ff1ea925c62e171121
nexhis <nexhi.sula@slu.edu>
2022-12-02 14:40:52 -0600

    adding some comments

--- elibrary/models.py
+++ elibrary/models.py
@@ -9,6 +9,7 @@ from users.models import Profile
 import datetime
 
 
+
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
@@ -28,7 +29,7 @@ class Books(models.Model):
     def __str__(self):
         return self.title
 
-
+#creating the order table for the customer orders 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
 	# date_ordered = models.DateTimeField(auto_now_add=True)
@@ -38,12 +39,14 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)  #our id is an integer 
 
+	#the function below gets the total price of the books added in the cart
 	@property
 	def get_cart_total(self):
 		orderitems = self.orderitem_set.all()
 		total = sum([item.get_total for item in orderitems])
 		return total 
-		
+
+	#it calculates the total items in the cart 
 	@property
 	def get_cart_items(self):
 		orderitems = self.orderitem_set.all()
@@ -62,7 +65,7 @@ class OrderItem(models.Model):
 		total = self.product.price * self.quantity
 		return total
 
-
+#the information about the shipping address for the customers
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
