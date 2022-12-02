b6490330d95dcdc384dfd16ba6643494ff666e8c
preethiyetcharla <113061317+preethiyetcharla@users.noreply.github.com>
2022-12-02 13:32:50 -0600

    adding some files

---
+++ elibrary/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from elibrary import views
+from django.urls import path
+from elibrary import views  #importing the views methods for the welcome, home and about page
+
+#defining the path for the website main buttons
+
+urlpatterns = [
+    path('', views.welcome, name='library'),
+    path('home/', views.home, name='library-homepage'),
+    path('about/', views.about, name='library-about'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('back-to-home/', views.backtohome, name='back-to-home'),
+]
